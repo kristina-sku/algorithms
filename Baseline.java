8f83917dde4638a11d11891d38775131ada7c06c
Kristina Skutilova <kristina.skutilova@ucdconnect.ie>
2024-04-11 12:16:47 +0100

    create baseline sorts methods

--- Baseline.java
+++ Baseline.java
@@ -1,6 +1,21 @@
 public class Baseline {
 
     //quickSort
+
+    public static void quickSort(){
+
+    }
+
     //2-way merge sort
+
+    public static void mergeSort(){
+
+    }
+
     //bucketSort
+    public static void bucketSort(){
+
+    }
+
+
 }
