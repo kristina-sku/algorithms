2e5e7e44c0dd90a478ae8fce765da4bf6ae8c1b7
Kristina Skutilova <kristina.skutilova@ucdconnect.ie>
2024-04-11 12:17:19 +0100

    create proposed sorts methods

--- Proposed.java
+++ Proposed.java
@@ -1,4 +1,11 @@
 public class Proposed {
     //parallel quick sort PQSA
+    public static void pqsa(){
+
+    }
+
     //merging subarrays from quick sort MQSA
+    public static void mqsa(){
+
+    }
 }
