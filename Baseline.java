0b5f90dc37e0eb1480658469825cfabb1622e981
Kristina Skutilova <kristina.skutilova@ucdconnect.ie>
2024-04-11 12:15:51 +0100

    create classes

--- Baseline.java
+++ Baseline.java
@@ -1,2 +1,6 @@
 public class Baseline {
+
+    //quickSort
+    //2-way merge sort
+    //bucketSort
 }
--- Proposed.java
+++ Proposed.java
@@ -1,2 +1,4 @@
 public class Proposed {
+    //parallel quick sort PQSA
+    //merging subarrays from quick sort MQSA
 }
