e3d37afadf338495ee12b7d2fcf0239146e095b6
Kristina Skutilova <kristina.skutilova@ucdconnect.ie>
2024-04-11 12:38:23 +0100

    start Proposed

--- Proposed.java
+++ Proposed.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class Proposed {
     //parallel quick sort PQSA
     public static void pqsa(){
